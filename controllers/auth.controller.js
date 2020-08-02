297fed7b12fd61af88be2e9eb4090a4711dd1379
shivaluma <shivaluma@gmail.com>
2020-08-02 17:21:23 +0700

    Add swagger to project

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -27,11 +27,14 @@ exports.postSignUp = async (req, res) => {
     });
     res.status(201).json({ message: 'Create account successfully' });
   } catch (err) {
-    console.log(err.errors[0].path.split('.')[1]);
+    console.log(err.errors[0]);
+    const env = process.env.NODE_ENV || 'development';
     return res.status(400).json({
       message: 'Cannot create account',
       duplicate: {
-        [err.errors[0].path.split('.')[1]]: true,
+        [env == 'development'
+          ? err.errors[0].path.split('.')[1]
+          : err.errors[0].path]: true,
       },
     });
   }
