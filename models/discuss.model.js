644ba0f756841cdb65de7428c8dfe676fd3d5d32
crepp-tt <tan.thai.2t@gmail.com>
2020-08-03 22:11:46 +0700

    Add discuss vote model

--- models/discuss.model.js
+++ models/discuss.model.js
@@ -30,24 +30,6 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.TEXT("long"),
       allowNull: false,
     },
-    viewCount: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      field: "view_count",
-    },
-    upVote: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      field: "up_vote",
-    },
-    downVote: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      field: "down_vote",
-    },
   });
   Discuss.associate = (models) => {
     Discuss.belongsTo(models.User, { foreignKey: "authorId" });
