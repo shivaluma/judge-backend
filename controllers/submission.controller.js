d44901b47848b51c9bcf028d960ff134ea4a131f
shivaluma <shivalumagmail.com>
2020-08-16 13:08:44 +0700

    !Fix submission when runtime error

--- controllers/submission.controller.js
+++ controllers/submission.controller.js
@@ -2,8 +2,24 @@ const { Problem, Submission, Testcase } = require('../models');
 const { Op, literal, QueryTypes } = require('sequelize');
 const logger = require('../configs/logger').logger;
 const got = require('got');
+
+const getShortStatus = (status) => {
+  switch (status) {
+    case 'Accepted':
+      return 'AC';
+    case 'Wrong Answer':
+      return 'WA';
+    case 'Time Limit Exceeded':
+      return 'TLE';
+    case 'Runtime Error':
+      return 'RE';
+    default:
+      return 'RE';
+  }
+};
+
 exports.createSubmission = async (req, res) => {
-  const { user } = req.user;
+  const user = req.user;
   const { code, problemId, language } = req.body;
 
   const problem = await Problem.findByPk(problemId, {
@@ -37,8 +53,31 @@ exports.createSubmission = async (req, res) => {
     const result = await got.get(response.body).json();
     if (result.status !== 'Queued' && result.status !== 'Processing') {
       logger.info(result);
+      const submission = await Submission.create({
+        status: getShortStatus(result.status),
+        runtime: !isNaN(Number(result.time_used[0]))
+          ? Number(result.time_used[0]).toFixed(1)
+          : 0,
+        language: language,
+        userId: user.id,
+        problemId: problemId,
+      });
       clearTimeout(currentInterval);
-      return res.status(201).json(result);
+      return res.status(201).json({ result: result, submission: submission });
     }
   }, 400);
+};
+
+exports.getSubmissions = async (req, res) => {
+  const user = req.user;
+  const { problemId } = req.query;
+  const submissions = await Submission.findAll({
+    where: {
+      userId: user.id,
+      problemId: problemId,
+    },
+    order: [['createdAt', 'DESC']],
+  });
+
+  return res.status(200).json({ submissions: submissions });
 };
