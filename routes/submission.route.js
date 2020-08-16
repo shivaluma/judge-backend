e086745bc4a0732bb4a010c702322e54ed7992ad
shivaluma <shivalumagmail.com>
2020-08-16 13:08:44 +0700

    Fix submission when runtime error

--- routes/submission.route.js
+++ routes/submission.route.js
@@ -5,5 +5,5 @@ const authenticate = passport.authenticate('jwt', { session: false });
 const submissionController = require('../controllers/submission.controller');
 
 router.post('/', authenticate, submissionController.createSubmission);
-
+router.get('/', authenticate, submissionController.getSubmissions);
 module.exports = router;
